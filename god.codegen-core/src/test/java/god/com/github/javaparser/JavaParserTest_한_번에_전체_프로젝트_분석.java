d3d116de5a5e61e7ffcabfb9fc111718cf48a646
이백행 <dlqorgod@naver.com>
2021-06-19 10:11:42 +0900

    JavaParser - javaparser get class name

--- god.codegen-core/src/test/java/god/com/github/javaparser/JavaParserTest_한_번에_전체_프로젝트_분석.java
+++ god.codegen-core/src/test/java/god/com/github/javaparser/JavaParserTest_한_번에_전체_프로젝트_분석.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.utils.SourceRoot;
 
 import lombok.extern.slf4j.Slf4j;
@@ -37,11 +39,33 @@ public class JavaParserTest_한_번에_전체_프로젝트_분석 {
 //				log.debug("parseResult={}", parseResult);
 
 				parseResult.getResult().ifPresent(cu -> {
-					log.debug("cu={}", cu);
+//					log.debug("cu={}", cu);
+					cu.getPrimaryTypeName().ifPresent(primaryTypeName -> {
+						log.debug("primaryTypeName={}", primaryTypeName);
+
+						Optional<ClassOrInterfaceDeclaration> coidOptional = cu.getClassByName(primaryTypeName);
+						coidOptional.ifPresent(coid -> {
+							coid.getMethods().forEach(method -> {
+//								log.debug("method={}", method);
+
+								log.debug("getNameAsString={}", method.getNameAsString());
+
+								method.getParameters().forEach(parameter -> {
+									log.debug("parameter={}", parameter);
+								});
+
+								log.debug("getType={}", method.getType());
+
+								method.getJavadoc().ifPresent(javadoc -> {
+									log.debug("getDescription={}", javadoc.getDescription().toText());
+								});
+							});
+						});
+					});
 				});
 
 				parseResult.getProblems().forEach(problem -> {
-					log.debug("problem={}", problem);
+//					log.debug("problem={}", problem);
 				});
 
 				parseResult.getCommentsCollection().ifPresent(cc -> {
@@ -52,10 +76,9 @@ public class JavaParserTest_한_번에_전체_프로젝트_분석 {
 //					cc.getBlockComments().forEach(comment -> {
 //						log.debug("comment={}", comment);
 //					});
-					cc.getJavadocComments().forEach(comment -> {
-						log.debug("comment={}", comment);
-					});
-					;
+//					cc.getJavadocComments().forEach(comment -> {
+//						log.debug("comment={}", comment);
+//					});
 				});
 			}
 
