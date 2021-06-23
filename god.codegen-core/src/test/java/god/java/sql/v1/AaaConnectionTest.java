8638fb5b0804789b2403234262202216e01e6f35
이백행 <dlqorgod@naver.com>
2021-06-23 21:45:15 +0900

    JavaParser - ClassOrInterfaceDeclaration.getMethods

--- god.codegen-core/src/test/java/god/java/sql/v1/AaaConnectionTest.java
+++ god.codegen-core/src/test/java/god/java/sql/v1/AaaConnectionTest.java
@@ -1,8 +1,10 @@
 package god.java.sql.v1;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLWarning;
 
 import org.junit.Test;
 
@@ -22,6 +24,12 @@ public class AaaConnectionTest {
 			ocon.setRemarksReporting(true);
 		}
 
+//		aaa(con);
+//		aab(con);
+		aac(con);
+	}
+
+	void aaa(Connection con) throws SQLException {
 		log.debug("con={}", con);
 		log.debug("getCatalog={}", con.getCatalog());
 		log.debug("getClass={}", con.getClass());
@@ -34,7 +42,9 @@ public class AaaConnectionTest {
 		log.debug("getTypeMap={}", con.getTypeMap());
 		log.debug("getWarnings={}", con.getWarnings());
 		log.debug("");
+	}
 
+	void aab(Connection con) throws SQLException {
 		log.debug("getAutoCommit={}", con.getAutoCommit());
 		log.debug("getMetaData={}", con.getMetaData());
 		log.debug("getCatalog={}", con.getCatalog());
@@ -46,6 +56,38 @@ public class AaaConnectionTest {
 		log.debug("getClientInfo={}", con.getClientInfo());
 //		log.debug("getSchema={}", con.getSchema());
 //		log.debug("getNetworkTimeout={}", con.getNetworkTimeout());
+	}
+
+	void aac(Connection con) throws SQLException {
+		boolean autoCommit = con.getAutoCommit();
+		boolean isClosed = con.isClosed();
+		DatabaseMetaData metaData = con.getMetaData();
+		boolean isReadOnly = con.isReadOnly();
+		String catalog = con.getCatalog();
+		int transactionIsolation = con.getTransactionIsolation();
+		SQLWarning warnings = con.getWarnings();
+		java.util.Map<String, Class<?>> typeMap = con.getTypeMap();
+		int holdability = con.getHoldability();
+//		boolean isValid = con.isValid();
+//		String clientInfo = con.getClientInfo();
+//		Properties clientInfo = con.getClientInfo();
+//		String schema = con.getSchema();
+//		int networkTimeout = con.getNetworkTimeout();
+
+		log.debug("autoCommit={}", autoCommit);
+		log.debug("isClosed={}", isClosed);
+		log.debug("metaData={}", metaData);
+		log.debug("isReadOnly={}", isReadOnly);
+		log.debug("catalog={}", catalog);
+		log.debug("transactionIsolation={}", transactionIsolation);
+		log.debug("warnings={}", warnings);
+		log.debug("typeMap={}", typeMap);
+		log.debug("holdability={}", holdability);
+//		log.debug("isValid={}", isValid);
+//		log.debug("clientInfo={}", clientInfo);
+//		log.debug("clientInfo={}", clientInfo);
+//		log.debug("schema={}", schema);
+//		log.debug("networkTimeout={}", networkTimeout);
 	}
 
 }
