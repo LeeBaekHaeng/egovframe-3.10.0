021d3c69dfc36f7b99e5cfb22e765e807eadbd3a
이백행 <dlqorgod@naver.com>
2021-06-29 07:40:29 +0900

    JavaParser - VO getFields

--- god.codegen-core/src/test/java/god/com/github/javaparser/JavaParserTest_AAA_VO.java
+++ god.codegen-core/src/test/java/god/com/github/javaparser/JavaParserTest_AAA_VO.java
@@ -73,6 +73,73 @@ public class JavaParserTest_AAA_VO {
 	}
 
 	@Test
+	public void test3() throws IOException {
+		String first = "src/main/java/egovframework/com/cmm/ComDefaultCodeVO.java";
+
+		CompilationUnit cu = StaticJavaParser.parse(Paths.get(first));
+
+		StringBuffer sb = new StringBuffer();
+
+		cu.getPrimaryTypeName().ifPresent(className -> {
+			cu.getClassByName(className).ifPresent(coid -> {
+
+				coid.getFields().forEach(field -> {
+					VariableDeclarator variable = field.getVariable(0);
+					String fieldType = variable.getTypeAsString();
+					String fieldName = variable.getNameAsString();
+
+					sb.append(fieldType);
+					sb.append(" ");
+					sb.append(fieldName);
+					sb.append(" = ");
+					if ("byte".equals(fieldType)) {
+						sb.append("0");
+					} else if ("short".equals(fieldType)) {
+						sb.append("0");
+					} else if ("int".equals(fieldType)) {
+						sb.append("0");
+					} else if ("long".equals(fieldType)) {
+						sb.append("0L");
+					} else if ("float".equals(fieldType)) {
+						sb.append("0.0f");
+					} else if ("double".equals(fieldType)) {
+						sb.append("0.0d");
+					} else if ("char".equals(fieldType)) {
+						sb.append("'\u0000'");
+					} else if ("String".equals(fieldType)) {
+//						sb.append("null");
+//						sb.append("\"\"");
+						if (field.getJavadoc().isPresent()) {
+							sb.append("\"");
+							sb.append(field.getJavadoc().get().getDescription().toText());
+							sb.append("\"");
+
+						} else {
+							sb.append("\"\"");
+						}
+					} else if ("boolean".equals(fieldType)) {
+						sb.append("false");
+					} else {
+						sb.append("\"\"");
+					}
+
+					sb.append(";");
+
+					field.getJavadoc().ifPresent(consumer -> {
+						sb.append(" // ");
+						sb.append(consumer.getDescription().toText());
+					});
+
+					sb.append("\n");
+				});
+
+			});
+		});
+
+		System.out.println(sb);
+	}
+
+	@Test
 	public void test2() throws IOException {
 		String first = "src/main/java/egovframework/com/cmm/ComDefaultCodeVO.java";
 
@@ -124,6 +191,25 @@ public class JavaParserTest_AAA_VO {
 		String tableNm = "";
 		String haveDetailCondition = ""; // 상세 조건 여부
 		String detailCondition = ""; // 상세 조건
+
+		ComDefaultCodeVO vo = new ComDefaultCodeVO();
+		vo.setCodeId(codeId); // 코드 ID
+		vo.setCode(code); // 상세코드
+		vo.setCodeNm(codeNm); // 코드명
+		vo.setCodeDc(codeDc); // 코드설명
+		vo.setTableNm(tableNm);
+		vo.setHaveDetailCondition(haveDetailCondition); // 상세 조건 여부
+		vo.setDetailCondition(detailCondition); // 상세 조건
+	}
+
+	void testb() {
+		String codeId = "코드 ID"; // 코드 ID
+		String code = "상세코드"; // 상세코드
+		String codeNm = "코드명"; // 코드명
+		String codeDc = "코드설명"; // 코드설명
+		String tableNm = "";
+		String haveDetailCondition = "상세 조건 여부"; // 상세 조건 여부
+		String detailCondition = "상세 조건"; // 상세 조건
 
 		ComDefaultCodeVO vo = new ComDefaultCodeVO();
 		vo.setCodeId(codeId); // 코드 ID
