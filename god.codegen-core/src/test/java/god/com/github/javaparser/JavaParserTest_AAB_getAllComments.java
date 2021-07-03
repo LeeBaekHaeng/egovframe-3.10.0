234b69335530c2f43d7ec68eaca4cee2e96e5192
이백행 <dlqorgod@naver.com>
2021-07-03 18:24:29 +0900

    JavaParser - CompilationUnit.getAllComments

---
+++ god.codegen-core/src/test/java/god/com/github/javaparser/JavaParserTest_AAB_getAllComments.java
@@ -0,0 +1,36 @@
+package god.com.github.javaparser;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JavaParserTest_AAB_getAllComments {
+
+	@Test
+	public void test() throws IOException {
+		String first = "src/main/java/egovframework/com/cmm/ComDefaultCodeVO.java";
+
+		CompilationUnit cu = StaticJavaParser.parse(Paths.get(first));
+
+		cu.getAllComments().forEach(comment -> {
+//			log.debug("comment={}", comment);
+
+			comment.toBlockComment().ifPresent(consumer -> {
+				log.debug("toBlockComment={}", consumer);
+			});
+
+			comment.toJavadocComment().ifPresent(consumer -> {
+//				log.debug("toJavadocComment={}", consumer);
+				log.debug("toText={}", consumer.parse().toText());
+			});
+		});
+	}
+
+}
