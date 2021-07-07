8685a6b5d2b09db0c1e11151f6ec8f05dc46dc60
이백행 <dlqorgod@naver.com>
2021-07-07 21:32:26 +0900

    JavaParser - CompilationUnit.getAllComments asJavadocComment

--- god.codegen-core/src/test/java/god/com/github/javaparser/JavaParserTest_AAB_getAllComments.java
+++ god.codegen-core/src/test/java/god/com/github/javaparser/JavaParserTest_AAB_getAllComments.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.javadoc.Javadoc;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,45 +23,49 @@ public class JavaParserTest_AAB_getAllComments {
 		cu.getAllComments().forEach(comment -> {
 //			log.debug("comment={}", comment);
 
-			log.debug("getContent={}", comment.getContent());
+//			log.debug("getContent={}", comment.getContent());
 
-			log.debug("isLineComment={}", comment.isLineComment());
+//			log.debug("isLineComment={}", comment.isLineComment());
 
 //			log.debug("getCommentedNode={}", comment.getCommentedNode());
 
 //			comment.asLineComment();
 
-			comment.getCommentedNode().ifPresent(commentedNode -> {
-				log.debug("commentedNode={}", commentedNode);
-			});
+//			comment.getCommentedNode().ifPresent(commentedNode -> {
+//				log.debug("commentedNode={}", commentedNode);
+//			});
 
-			log.debug("isOrphan={}", comment.isOrphan());
+//			log.debug("isOrphan={}", comment.isOrphan());
 
-			log.debug("getMetaModel={}", comment.getMetaModel());
+//			log.debug("getMetaModel={}", comment.getMetaModel());
 
-			log.debug("isBlockComment={}", comment.isBlockComment());
+//			log.debug("isBlockComment={}", comment.isBlockComment());
 
 //			log.debug("asBlockComment={}", comment.asBlockComment());
 
 			boolean isJavadocComment = comment.isJavadocComment();
-			log.debug("isJavadocComment={}", isJavadocComment);
+//			log.debug("isJavadocComment={}", isJavadocComment);
 			if (isJavadocComment) {
-				log.debug("asJavadocComment={}", comment.asJavadocComment());
+//				log.debug("asJavadocComment={}", comment.asJavadocComment());
+				Javadoc parse = comment.asJavadocComment().parse();
+//				log.debug("parse={}", parse);
+//				log.debug("toText={}", parse.toText());
+				log.debug("getDescription={}", parse.getDescription().toText());
 			}
 
-			comment.toBlockComment().ifPresent(consumer -> {
-				log.debug("toBlockComment={}", consumer);
-			});
+//			comment.toBlockComment().ifPresent(consumer -> {
+//				log.debug("toBlockComment={}", consumer);
+//			});
 
-			comment.toJavadocComment().ifPresent(consumer -> {
-				log.debug("toJavadocComment={}", consumer);
-//				log.debug("toText={}", consumer.parse().toText());
-//				log.debug("getDescription={}", consumer.parse().getDescription().toText());
-			});
+//			comment.toJavadocComment().ifPresent(consumer -> {
+//				log.debug("toJavadocComment={}", consumer);
+////				log.debug("toText={}", consumer.parse().toText());
+////				log.debug("getDescription={}", consumer.parse().getDescription().toText());
+//			});
 
-			comment.toLineComment().ifPresent(consumer -> {
-				log.debug("toLineComment={}", consumer);
-			});
+//			comment.toLineComment().ifPresent(consumer -> {
+//				log.debug("toLineComment={}", consumer);
+//			});
 		});
 	}
 
