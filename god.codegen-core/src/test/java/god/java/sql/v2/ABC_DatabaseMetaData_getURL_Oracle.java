ddc47ea587831dc97f3e3bb37e33d950c4ddaf3b
이백행 <dlqorgod@naver.com>
2021-07-13 05:25:24 +0900

    Code Generation 코드 생성 - DatabaseMetaData.getURL

---
+++ god.codegen-core/src/test/java/god/java/sql/v2/ABC_DatabaseMetaData_getURL_Oracle.java
@@ -0,0 +1,31 @@
+package god.java.sql.v2;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import egovframework.com.cmm.util.EgovResourceCloseHelper;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ABC_DatabaseMetaData_getURL_Oracle {
+
+	@Test
+	public void test() {
+		Connection conn = GodDriverManagerTest.getConnectionOracle();
+		try {
+			DatabaseMetaData dmd = conn.getMetaData();
+
+			String url = dmd.getURL();
+
+			log.debug("url={}", url);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+		} finally {
+			EgovResourceCloseHelper.closeDBObjects(conn);
+		}
+	}
+
+}
