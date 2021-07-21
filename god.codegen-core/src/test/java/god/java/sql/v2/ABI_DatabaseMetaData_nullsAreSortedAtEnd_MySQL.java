b967bba8a8432ec7636d9ee2393267cdf06d7f59
이백행 <dlqorgod@naver.com>
2021-07-21 21:43:20 +0900

    Code Generation 코드 생성 - DatabaseMetaData.nullsAreSortedAtEnd

---
+++ god.codegen-core/src/test/java/god/java/sql/v2/ABI_DatabaseMetaData_nullsAreSortedAtEnd_MySQL.java
@@ -0,0 +1,31 @@
+package god.java.sql.v2;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import egovframework.com.cmm.util.EgovResourceCloseHelper;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ABI_DatabaseMetaData_nullsAreSortedAtEnd_MySQL {
+
+	@Test
+	public void test() {
+		Connection conn = GodDriverManagerTest.getConnectionMySQL();
+		try {
+			DatabaseMetaData dmd = conn.getMetaData();
+
+			boolean nullsAreSortedAtEnd = dmd.nullsAreSortedAtEnd();
+
+			log.debug("nullsAreSortedAtEnd={}", nullsAreSortedAtEnd);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+		} finally {
+			EgovResourceCloseHelper.closeDBObjects(conn);
+		}
+	}
+
+}
