460234b042ae5af85e4a74e659a77038c2d14a99
이백행 <dlqorgod@naver.com>
2021-07-23 20:50:24 +0900

    Code Generation 코드 생성 - DatabaseMetaData.getDatabaseProductVersion

---
+++ god.codegen-core/src/test/java/god/java/sql/v2/ABK_DatabaseMetaData_getDatabaseProductName_MySQL.java
@@ -0,0 +1,31 @@
+package god.java.sql.v2;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import egovframework.com.cmm.util.EgovResourceCloseHelper;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ABK_DatabaseMetaData_getDatabaseProductName_MySQL {
+
+	@Test
+	public void test() {
+		Connection conn = GodDriverManagerTest.getConnectionMySQL();
+		try {
+			DatabaseMetaData dmd = conn.getMetaData();
+
+			String databaseProductVersion = dmd.getDatabaseProductVersion();
+
+			log.debug("databaseProductVersion={}", databaseProductVersion);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+		} finally {
+			EgovResourceCloseHelper.closeDBObjects(conn);
+		}
+	}
+
+}
