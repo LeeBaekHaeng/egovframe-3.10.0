0e742db89c083c1d70cce6342a1bea050299b872
이백행 <dlqorgod@naver.com>
2021-07-26 21:11:15 +0900

    JavaParser - CompilationUnit.getPrimaryType

---
+++ god.codegen-core/src/test/java/god/com/github/javaparser/JavaParserTest_AAL_getPrimaryType.java
@@ -0,0 +1,34 @@
+package god.com.github.javaparser;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JavaParserTest_AAL_getPrimaryType {
+
+	@Test
+	public void test() {
+		String first = "src/main/java/egovframework/com/cmm/ComDefaultCodeVO.java";
+
+		CompilationUnit compilationUnit = null;
+
+		try {
+			compilationUnit = StaticJavaParser.parse(Paths.get(first));
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+
+		compilationUnit.getPrimaryType().ifPresent(primaryType -> {
+//			log.debug("primaryType={}", primaryType);
+			log.debug("primaryType.getName().getIdentifier()={}", primaryType.getName().getIdentifier());
+		});
+	}
+
+}
