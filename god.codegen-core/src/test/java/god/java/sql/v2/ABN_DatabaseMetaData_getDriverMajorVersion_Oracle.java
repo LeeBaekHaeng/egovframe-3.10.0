f3bcb0c953e94bdce0dfd50a34f5c74f49077794
이백행 <dlqorgod@naver.com>
2021-07-28 21:27:16 +0900

    Code Generation 코드 생성 - DatabaseMetaData.getDriverMajorVersion

---
+++ god.codegen-core/src/test/java/god/java/sql/v2/ABN_DatabaseMetaData_getDriverMajorVersion_Oracle.java
@@ -0,0 +1,31 @@
+package god.java.sql.v2;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import egovframework.com.cmm.util.EgovResourceCloseHelper;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ABN_DatabaseMetaData_getDriverMajorVersion_Oracle {
+
+	@Test
+	public void test() {
+		Connection conn = GodDriverManagerTest.getConnectionOracle();
+		try {
+			DatabaseMetaData dmd = conn.getMetaData();
+
+			int driverMajorVersion = dmd.getDriverMajorVersion();
+
+			log.debug("driverMajorVersion={}", driverMajorVersion);
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+		} finally {
+			EgovResourceCloseHelper.closeDBObjects(conn);
+		}
+	}
+
+}
