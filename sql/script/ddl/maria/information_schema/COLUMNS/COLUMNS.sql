e7206001b25e7b3a0c513d7ac0a6397c7d11035e
이백행 <dlqorgod@naver.com>
2021-11-18 07:24:30 +0900

    [별지 10] 테이블정의서(공공기관의 데이터베이스 표준화 지침).hwp

---
+++ sql/script/ddl/maria/information_schema/COLUMNS/COLUMNS.sql
@@ -0,0 +1,20 @@
+select
+    A.*
+from
+    information_schema.COLUMNS A
+where 1 = 1
+--     and TABLE_SCHEMA = 'information_schema'
+    and TABLE_SCHEMA = 'com'
+;
+
+/*
+컬럼정의서
+*/
+select
+    A.*
+from
+    information_schema.COLUMNS A
+where 1 = 1
+--     and TABLE_SCHEMA = 'information_schema'
+    and TABLE_SCHEMA = 'com'
+;
--- sql/script/ddl/maria/information_schema/information_schema.sql
+++ sql/script/ddl/maria/information_schema/information_schema.sql
@@ -1,4 +1,24 @@
 select
     A.*
-from information_schema.ALL_PLUGINS A
+from
+    information_schema.ALL_PLUGINS A
+;
+
+select
+    A.*
+from
+    information_schema.VIEWS A
+where 1 = 1
+--     and A.TABLE_CATALOG = 'def'
+-- 
+--     and A.TABLE_SCHEMA = 'mysql'
+--     and A.TABLE_SCHEMA = 'sys'
+-- 
+--     and TABLE_NAME = 'VIEWS'
+;
+
+select A.TABLE_CATALOG, count(*) cnt from information_schema.VIEWS A group by TABLE_CATALOG
+;
+
+select A.TABLE_SCHEMA, count(*) cnt from information_schema.VIEWS A group by TABLE_SCHEMA
 ;
